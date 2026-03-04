d7c9c002e9340eeab72ed91e4680c06d30c0dc7f
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-03-04 11:59:49 -0300

    PUT User

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -35,6 +35,18 @@ public class UserService {
         return new UserDTO(entity);
     }
 
+    public UserDTO update(String id, UserDTO dto) {
+        User entity = getEntityById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new UserDTO(entity);
+    }
+
+    private User getEntityById(String id) {
+        Optional<User> result = repository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+    }
+
     private void copyDtoToEntity(UserDTO dto, User entity) {
         entity.setName(dto.getName());
         entity.setEmail(dto.getEmail());
